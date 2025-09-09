26cef40f3502d6baf05165216c2901a78fb79c5c
Johnathan McAllister <doorman.jm@gmail.com>
2025-09-09 19:44:41 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/***********************************************************************************************************************
+ * CEN 3024C - Software Development 1
+ * Professor: Dr. Lisa Macon
+ * Course ID: 202610
+ * CRN: 14877
+ * Semester: Fall 2025
+ *
+ * Date: 09/09/25
+ * Written by: Johnathan McAllister /s
+ * Purpose: Library Management System
+ *
+ ***********************************************************************************************************************
+ */
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
