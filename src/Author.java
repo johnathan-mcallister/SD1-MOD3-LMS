41b2994898d14b59bf974a864140ecd703cc4906
Johnathan McAllister <doorman.jm@gmail.com>
2025-09-09 20:48:40 -0400

    Update Author.java

--- src/Author.java
+++ src/Author.java
@@ -1,5 +1,23 @@
 import java.util.ArrayList;
 
-public class Author {
+public class Author extends Person {
     private ArrayList<Book> bodyOfWork;
+
+    public Author(String firstName, String lastName, String phoneNumber, Address address, ArrayList<Book> bodyOfWork) {
+        super(firstName, lastName, phoneNumber, address);
+        this.bodyOfWork = new ArrayList<Book>(bodyOfWork);
+    }
+
+    public ArrayList<Book> getBodyOfWork() {
+        return bodyOfWork;
+    }
+
+    public void setBodyOfWork(ArrayList<Book> bodyOfWork) {
+        this.bodyOfWork = bodyOfWork;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
