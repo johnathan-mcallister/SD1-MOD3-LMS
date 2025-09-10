c06dfef82c0bda6c14a905062eac9694f7298ca2
Johnathan McAllister <doorman.jm@gmail.com>
2025-09-09 20:16:09 -0400

    Create Person.java

---
+++ src/Person.java
@@ -0,0 +1,47 @@
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private Address address;
+
+    public Person(String fName, String lName, String phone, Address address) {
+        this.firstName = fName;
+        this.lastName = lName;
+        this.phoneNumber = phone;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+}
