e610538eb3720e5815644d35fd9971ce663c7348
Johnathan McAllister <doorman.jm@gmail.com>
2025-09-09 20:29:57 -0400

    Create Patron.java

---
+++ src/Patron.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Patron extends Person{
+
+    private String UID;
+    private ArrayList<Book> bookList;
+
+    public Patron(String firstName, String lastName, String phoneNumber, Address address, String UID,  ArrayList<Book> bookList) {
+        super(firstName, lastName, phoneNumber, address);
+        this.UID = UID;
+        this.bookList = new ArrayList<Book>();
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+
+    public ArrayList<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(ArrayList<Book> bookList) {
+        this.bookList = bookList;
+    }
+}
