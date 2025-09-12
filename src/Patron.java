d05d54973f1eca6eb7fa103bff921d242f58222b
Johnathan McAllister <doorman.jm@gmail.com>
2025-09-11 21:29:33 -0400

    Update Patron.java

--- src/Patron.java
+++ src/Patron.java
@@ -1,26 +1,60 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Patron extends Person{
 
     private String UID;
+    private double odFine;
     private ArrayList<Book> bookList;
+    final Scanner input = new Scanner(System.in);
+    final DecimalFormat df = new DecimalFormat("0.00");
 
-    public Patron(String firstName, String lastName, String phoneNumber, Address address, String UID,  ArrayList<Book> bookList) {
+    public Patron(String firstName, String lastName, String phoneNumber, Address address, ArrayList<Patron> patronList) {
         super(firstName, lastName, phoneNumber, address);
-        this.UID = UID;
-        this.bookList = new ArrayList<Book>();
+        this.UID = generateUID(patronList);
+        this.odFine = 0.0;
+        this.bookList = new ArrayList<>();
     }
 
     public String getUID() {
-        return UID;
+        return this.UID;
     }
 
-    public void setUID(String UID) {
-        this.UID = UID;
+    public String generateUID(ArrayList<Patron> patronList) {
+        if (patronList == null) {
+            return "LP00001";
+        } else {
+            int max = Integer.parseInt(patronList.getFirst().UID);
+            for (Patron patron : patronList) {
+                if (Integer.parseInt(patron.getUID()) > max) {
+                    max = Integer.parseInt(patron.getUID());
+                }
+            }
+            max += 1;
+            return String.format("LP%05d", max);
+        }
+    }
+
+    public void setUID(ArrayList<Patron> patronList) {
+        this.UID = generateUID(patronList);
+    }
+
+    public double getODFine() {
+        return odFine;
+    }
+
+    public void setODFine(double odFine) {
+        this.odFine = odFine;
+    }
+
+    public void addODFine(double odFine) {
+        this.odFine += odFine;
     }
 
     public ArrayList<Book> getBookList() {
-        return bookList;
+        return this.bookList;
     }
 
     public void setBookList(ArrayList<Book> bookList) {
@@ -29,6 +63,10 @@ public class Patron extends Person{
 
     @Override
     public String toString() {
-        return "Patron{" + "UID=" + UID + ", bookList=" + bookList + '}';
+        return String.format("[%s] %s, %s - %s",this.UID, super.lastName, super.firstName, super.phoneNumber);
+    }
+
+    public String toStringFull() {
+        return String.format("UID: %s\nNAME: %s, %s\nPHONE: %s\n%s\nBALANCE: $%s\nBOOKLIST: %s",this.UID, super.lastName, super.firstName, super.phoneNumber, super.address, df.format(this.odFine), this.bookList);
     }
 }
